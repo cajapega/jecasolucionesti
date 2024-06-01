7aaf26bd0bc3f499750e275d3238a8c21ce251b1
Carlos J. Pérez <charlyjun@gmail.com>
2024-06-01 10:16:41 -0500

    Se prepara la estructura basica que va a tener el sitio Web, todos los archivos estan vacios excepto typography.css y reset.css tienen estilos basicos, el archivo js tiene una unica funcion que sirve para el icono de hamburger para pantallas celulares, el index.html tiene la estructura basica con la estructura de navbar tipica

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById('hamburger').onclick = function() {
+    var navLinks = document.getElementById('nav-links');
+    if (navLinks.style.display === 'flex') {
+        navLinks.style.display = 'none';
+    } else {
+        navLinks.style.display = 'flex';
+    }
+}
