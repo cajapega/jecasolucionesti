af6d791047c2975cafbae75510ad6d57528e2cb3
Carlos J. Pérez <charlyjun@gmail.com>
2024-06-03 21:26:02 -0500

    Se mejora el navbar en ese caso a traves de js se inyecta HTML para segun el tamano de la pantalla muestre informacion de contacto en el navbar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+/*Hamburger*/
 document.getElementById('hamburger').onclick = function() {
     var navLinks = document.getElementById('nav-links');
     if (navLinks.style.display === 'flex') {
@@ -7,3 +8,56 @@ document.getElementById('hamburger').onclick = function() {
         navLinks.style.display = 'flex';
     }
 }
+
+/*Script para insertar html*/
+document.addEventListener('DOMContentLoaded', function() {
+    function checkWindowSize() {
+        var navbar = document.getElementById("navbar");
+        var contactInfo = document.querySelector(".contactInfo");
+
+        if (window.innerWidth > 768) {
+            if (!contactInfo) {
+                var newContactInfo = document.createElement('div');
+                newContactInfo.classList.add('contactInfo');
+                newContactInfo.innerHTML = `
+                    <p>Siguenos en:</p>
+                    <a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook"></i></a>
+                    <a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.tiktok.com" target="_blank"><i class="fab fa-tiktok"></i></a>
+                    <p>Escribanos:</p>
+                    <a href="https://www.tiktok.com" target="_blank"><i class="fa-solid fa-envelope"></i></a>
+                    <p>Converse con nosotros:</p>
+                    <a href="https://www.tiktok.com" target="_blank"><i class="fa-brands fa-whatsapp"></i></a>
+                `;
+                navbar.insertBefore(newContactInfo, navbar.firstChild);
+            }
+        } else {
+            if (contactInfo) {
+                contactInfo.parentNode.removeChild(contactInfo);
+            }
+        }
+    }
+
+    // Ejecutar al cargar la página
+    checkWindowSize();
+    
+    // Ejecutar al redimensionar la ventana
+    window.addEventListener('resize', checkWindowSize);
+});
+
+/*Scrol contact info y navbar*/
+window.onscroll = function() {
+    var navbar = document.getElementById("navbar");
+    var contactInfo = document.querySelector(".contactInfo");
+    var mainNav = document.querySelector(".mainNav");
+    if (window.scrollY > contactInfo.offsetHeight) {
+        contactInfo.style.opacity = "0";
+        contactInfo.style.height = "0";
+        mainNav.style.top = "0";
+    } else {
+        contactInfo.style.opacity = "1";
+        contactInfo.style.height = "5vh";
+        mainNav.style.top = "5vh";
+    }
+};
+
