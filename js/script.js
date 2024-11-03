530ec359f6286ce90dcc909da4421e836884f742
Carlos J. <charlyjun@gmail.com>
2024-11-03 07:26:08 -0500

    Se termina seccion hero2, se realizan ajustes en el codigo ya que una etiqueta mal referenciada <p> del hero 2, estaba afectando la visibilidad de las etiquetas <p> del navbar contact info

--- js/script.js
+++ js/script.js
@@ -94,3 +94,30 @@ setInterval(() => {
     showSlide(currentIndex);
 }, 5000); // Cambiar de slide cada 5 segundos
 
+//Formulario EmailJs
+
+const btn = document.getElementById('button-form');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_zb9lta5';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+//Boton de chat
+document.getElementById("chatButton").addEventListener("click", function() {
+    window.location.href = "https://wa.me/593967399852";
+});
